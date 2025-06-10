16e9d7d9f2e32f34d2d508a02e0ebdcefa4a008b
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-10 14:32:11 +0530

    Fourth problem solution/Code

---
+++ Assisted04.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Assisted04{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a radius of a circle: ");
+		double radius = sc.nextDouble();
+		sc.close();
+		double area = radius * radius * Math.PI;
+		System.out.println(area);
+	}
+}	
