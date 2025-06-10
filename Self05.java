29c5998f282a1188687cf8651b16d4159709d279
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-10 15:03:48 +0530

    Convert Kilometers to Miles

---
+++ Self05.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Self05{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		double Kilometers = sc.nextDouble();
+		double Miles = Kilometers * 0.621371;
+		System.out.println(Miles);
+	}
+}
