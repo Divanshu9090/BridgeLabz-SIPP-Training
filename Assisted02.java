2c98ca4c2d6d6ac71d6e7969fb37226f80f81ffc
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-10 11:22:20 +0530

    Second problem solution/Code

---
+++ Assisted02.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Assisted02{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		sc.close();
+		int addition = num1+num2;
+		System.out.println(addition);
+	}
+}
