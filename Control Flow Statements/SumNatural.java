56969c49b25d9282e90cc93107718127679b2612
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-13 10:04:26 +0530

    [Divanshu] Add. Compute sum of n natural number

---
+++ Control Flow Statements/SumNatural.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class SumNatural{
+	public static void main (String [] args){
+		// create a Scanner object
+		Scanner input = new Scanner(System.in);
+		
+		//Get user input
+		int n = input.nextInt();
+		
+		//check number is natural or not
+		if(n>=0){
+			//Compute sum
+			long sum = n*(n+1)/2;
+			
+			System.out.println("The sum of "+n+" natural numbers is " + sum);
+		} else {
+			System.out.println("The number "+n+" is not a natural number");
+		}
+		
+		//close Scanner stream
+		input.close();
+	}
+}
