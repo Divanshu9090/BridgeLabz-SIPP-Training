3e7d0979783d88ad2112af57d2891f986cef0493
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-22 15:28:32 +0530

    [Divanshu] Add. program to check if a given string is a palindrome or not.

---
+++ Strings/Palindrome.java
@@ -0,0 +1,37 @@
+package Strings;
+
+import java.util.Scanner;
+public class Palindrome {
+	// Method to check String is Palindrome or not.
+	private static boolean checkPalindrome(String str) {
+		int l=0,r=str.length()-1;
+		while(l<r) {
+			if(str.charAt(r)!=str.charAt(l)) {
+				return false;
+			}
+			l++;
+			r--;
+		}
+		return true;
+	}
+
+	// Main Method
+	public static void main(String[] args) {
+		// Create a Scanner Object
+		Scanner input = new Scanner(System.in);
+		
+		//Take a user input
+		String str = input.nextLine();
+		
+		//  Reverse String and Display it;
+		boolean res = checkPalindrome(str);
+		if(res) {
+			System.out.println(str +" is Palindrome!");
+		}else{
+			System.out.println(str + " is not a Palindrome.");
+		}
+		
+		// close the Scanner
+		input.close();
+	}
+}
