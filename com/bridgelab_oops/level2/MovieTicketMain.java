c58ed314594091faff28d3ae51da3f1c403e6848
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-23 23:39:32 +0530

    [Divanshu] Add. Create a Main class for MovieTicket class.

---
+++ com/bridgelab_oops/level2/MovieTicketMain.java
@@ -0,0 +1,30 @@
+package com.bridgelab_oops.level2;
+
+import java.util.Scanner;
+
+public class MovieTicketMain {
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Create a MovieTicket object
+        MovieTicket ticket = new MovieTicket();
+
+        // Booking input
+        System.out.print("Enter Movie Name: ");
+        String movie = input.nextLine();
+
+        System.out.print("Enter Seat Number: ");
+        String seat = input.nextLine();
+
+        System.out.print("Enter Ticket Price: ");
+        double price = input.nextDouble();
+
+        // Book and display ticket
+        ticket.bookTicket(movie, seat, price);
+        ticket.displayTicket();
+
+        // Close the scanner
+        input.close();
+    }
+}
