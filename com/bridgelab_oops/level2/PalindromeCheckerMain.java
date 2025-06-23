81b28597166b091f5d78500cb8ddfa8f0acb0295
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-23 23:35:36 +0530

    [Divanshu] Add. Create a Main class for PalindromeChecker class.

---
+++ com/bridgelab_oops/level2/PalindromeCheckerMain.java
@@ -0,0 +1,21 @@
+package com.bridgelab_oops.level2;
+
+import java.util.Scanner;
+
+public class PalindromeCheckerMain {
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Take input from the user
+        System.out.print("Enter a string to check for palindrome: ");
+        String str = input.nextLine();
+
+        // Create an instance of PallindromeChecker and check the result
+        PalindromeChecker checker = new PalindromeChecker(str);
+        checker.displayResult();
+
+        // Close the Scanner
+        input.close();
+    }
+}
