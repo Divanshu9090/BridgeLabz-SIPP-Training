5a2db2c47a4e171b72e6f2c7ecd37d892fe0f407
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-06-26 12:51:05 +0530

    [Divanshu] Fix. code

--- com/keywords/shoppingcartsystem/Main.java
+++ com/keywords/shoppingcartsystem/Main.java
@@ -3,8 +3,22 @@ package com.keywords.shoppingcartsystem;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Creating product objects
+		Product p1 = new Product("P101", "Laptop", 50000, 1);
+		Product p2 = new Product("P102", "Smartphone", 20000, 2);
 
+		// Updating discount for all products
+		Product.updateDiscount(15.0);
+
+		// Validate with instanceof and display details
+		if (p1 instanceof Product) {
+			p1.displayProductDetails();
+		}
+
+		System.out.println(); // spacing
+
+		if (p2 instanceof Product) {
+			p2.displayProductDetails();
+		}
 	}
-
 }
