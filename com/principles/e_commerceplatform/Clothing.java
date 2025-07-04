9b28542a0c38845e7059777caf0a5c1a56ed50ea
Divanshu9090 <133981764+Divanshu9090@users.noreply.github.com>
2025-07-04 16:56:47 +0530

    [Divanshu] Add. Create a Clothing class which extends Product and implements Taxable class.

---
+++ com/principles/e_commerceplatform/Clothing.java
@@ -0,0 +1,34 @@
+package com.principles.e_commerceplatform;
+
+class Clothing extends Product implements Taxable {
+	public Clothing(String productId, String name, double price) {
+		super(productId, name, price);
+	}
+
+	@Override
+	public double calculateDiscount() {
+		return getPrice() * 0.15; // 15% discount
+	}
+
+	@Override
+	public double calculateTax() {
+		return getPrice() * 0.05; // 5% tax
+	}
+
+	@Override
+	public String getTaxDetails() {
+		return "Clothing Tax: 5%";
+	}
+}
+
+// Groceries Product Class
+class Groceries extends Product {
+	public Groceries(String productId, String name, double price) {
+		super(productId, name, price);
+	}
+
+	@Override
+	public double calculateDiscount() {
+		return getPrice() * 0.05; // 5% discount
+	}
+}
